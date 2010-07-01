027156f570ec47709dbed412e93ff0c11467f6bd
David Medinets <davd.medinets@gmail.com>
2010-07-01 00:13:24 -0400

    allow the twitter username and password to be specfied in the model as well as a configuration file.

--- lib/alastrina.rb
+++ lib/alastrina.rb
@@ -1,7 +1,4 @@
-
 ALASTRINA_CONFIGURATION_FILE = 'config/alastrina.yml'
-
-throw "Missing #{ALASTRINA_CONFIGURATION_FILE}" unless File.exists? ALASTRINA_CONFIGURATION_FILE 
 
 module Alastrina
   def self.included(base)
@@ -9,31 +6,49 @@ module Alastrina
   end
   
   module ClassMethods
-    def alastrina
-      throw "Missing lock_version field." unless column_names.include? "lock_version"
+    def alastrina hash
+      p "1hash: #{hash.inspect}"
+      module_eval do
+        p "2hash: #{hash.inspect}"
+        def configuration
+          throw "Missing #{ALASTRINA_CONFIGURATION_FILE}" unless File.exists? ALASTRINA_CONFIGURATION_FILE 
+          @config ||= YAML::load(File.read(ALASTRINA_CONFIGURATION_FILE))
+        end
+        if hash[:twitter]
+          def send_to_twitter?
+            true
+          end
+          eval"def twitter_username\n\"#{hash[:twitter][:username]}\"\nend\n"
+          eval "def twitter_password\n\"#{hash[:twitter][:password]}\"\nend\n"
+        else
+          def send_to_twitter?
+            @twitter_flag ||= !configuration['twitter'].blank?
+          end
+          def twitter_username
+            @twitter_username ||= configuration['twitter']['username'] if send_to_twitter?
+          end
+          def twitter_password
+            @twitter_password ||= configuration['twitter']['password'] if send_to_twitter?
+          end
+        end
+      end
     end
   end
 
-  def before_save
-  end
-  
   def after_save
-    @@config ||= YAML::load(File.read(ALASTRINA_CONFIGURATION_FILE))
-    @@twitter_flag ||= !@@config['twitter'].nil?
-    
-    if @@twitter_flag
+    if send_to_twitter?
       require 'twitter' 
-      throw "Missing Twitter userid" if @@config['twitter']['username'].blank? 
-      throw "Missing Twitter password" if @@config['twitter']['password'].blank? 
+      throw "Missing Twitter userid" if twitter_username.blank? 
+      throw "Missing Twitter password" if twitter_password.blank?
+      send_via_twitter
     end
-    send_via_twitter if @@twitter_flag
   end
 
 private
 
   def send_via_twitter
     if changes.size > 0
-      httpauth = Twitter::HTTPAuth.new(@@config['twitter']['username'], @@config['twitter']['password'])
+      httpauth = Twitter::HTTPAuth.new(twitter_username, twitter_password)
       client = Twitter::Base.new(httpauth)
       begin
         client.update(changes.to_yaml)
